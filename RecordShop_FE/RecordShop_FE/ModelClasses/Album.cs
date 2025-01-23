5d987ed53c851292b5c0a11b41530ee26567e8d0
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-23 12:22:41 +0000

    Add project files.

---
+++ RecordShop_FE/RecordShop_FE/ModelClasses/Album.cs
@@ -0,0 +1,16 @@
+﻿namespace RecordShop
+{
+    public class Album
+    {
+        public int Id { get; set; } = default(int);
+        public string Title { get; set; }
+        public string Artist { get; set; }
+
+        public Album(int id, string title, string artist)
+        {
+            Id = id;
+            Title = title;
+            Artist = artist;
+        }
+    }
+}
