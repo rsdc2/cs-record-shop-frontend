14ce70d5410fa1b38c96e5e18ee759521b575eb6
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-23 15:05:57 +0000

    Implement edit record details

--- RecordShop_FE/RecordShop_FE/Program.cs
+++ RecordShop_FE/RecordShop_FE/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
-
+builder.Logging.SetMinimumLevel(LogLevel.Information);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
