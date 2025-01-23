5c801e934b38b6ee681e3a5417abdd868d27f5e7
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-23 16:57:33 +0000

    Makes changes on backend server

---
+++ RecordShop_FE/RecordShop_FE/HttpUtils/HttpUtils.cs
@@ -0,0 +1,28 @@
+﻿using RecordShop;
+using System.Net.Http;
+
+namespace RecordShop_FE
+{
+    public class HttpUtils
+    {
+        HttpClient _httpClient;
+
+        public HttpUtils()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public HttpResponseMessage UpdateRecord(Album album)
+        {
+            _httpClient = new HttpClient();
+            var content = JsonContent.Create<Album>(album);
+            var message = new HttpRequestMessage
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7085/Albums/{album.Id}"),
+                Content = content
+            };
+            return _httpClient.Send(message);
+        }
+    }
+}
