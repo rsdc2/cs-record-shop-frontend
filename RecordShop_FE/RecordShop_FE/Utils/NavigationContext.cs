71524c21021730ca7c43bdde5c1d2a61d65e4ce2
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-24 13:59:30 +0000

    Start implementing context manager

---
+++ RecordShop_FE/RecordShop_FE/Utils/NavigationContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace RecordShop;
+
+public class NavigationContext
+{
+    NavigationManager _navigationManager;
+
+    //public Page Page { 
+    //    get => _navigationManager.Uri switch
+    //    {
+
+    //    }
+            
+    //}
+
+    public NavigationContext(NavigationManager navigationManager)
+    {
+        _navigationManager = navigationManager;
+    }
+
+}
