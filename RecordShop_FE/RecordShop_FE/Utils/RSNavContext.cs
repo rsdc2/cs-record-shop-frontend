3ace7477e4dc25e3b00b6ac6dfdca8bae908d826
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-27 15:56:34 +0000

    Implement context tracking for navigation

---
+++ RecordShop_FE/RecordShop_FE/Utils/RSNavContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecordShop;
+
+public class RSNavContext
+{
+    NavigationManager _navigationManager;
+
+    public RSNavContext(NavigationManager navigationManager)
+    {
+        _navigationManager = navigationManager;
+    }
+
+    public void NavigateTo(string relativeUri)
+    {
+        _navigationManager.NavigateTo(_navigationManager.BaseUri + relativeUri);
+    }
+
+    public void NavigateBack(Page pageContext, Album? album = null)
+    {
+        switch (pageContext)
+        {
+            case Page.AllRecords:
+                NavigateTo("/records");
+                break;
+
+            case Page.SingleRecord:
+                NavigateTo($"/record/{album.Id}");
+                break;
+        }
+    }
+
+}
